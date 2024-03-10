2d1313e46f72f16d8c09a3380a1dd8097a1932dc
Autsada555 <autsadaheha@gmail.com>
2024-03-10 16:37:24 +0700

    first commit

---
+++ backend/entity/setup.go
@@ -0,0 +1,174 @@
+package entity
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func DB() *gorm.DB {
+	return db
+}
+
+func SetupDatabase() {
+	database, err := gorm.Open(sqlite.Open("PVegDB.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	database.AutoMigrate(
+		&Member{},
+	)
+db = database
+
+	password, _ := bcrypt.GenerateFromPassword([]byte("admin12345"), 14)
+
+	db.Model(&Member{}).Create(&Member{
+		Firstname:      "admin",
+		Lastname:       "admin",
+		Email:          "admin@admin.admin",
+		Password:       string(password),
+	})
+
+	// database.Where(HotelType{Name: "Hotel"}).FirstOrCreate(&HotelType{Name: "Hotel"})
+	// database.Where(HotelType{Name: "Resort"}).FirstOrCreate(&HotelType{Name: "Resort"})
+	// database.Where(HotelType{Name: "Villa"}).FirstOrCreate(&HotelType{Name: "Villa"})
+	// database.Where(HotelType{Name: "Apartment"}).FirstOrCreate(&HotelType{Name: "Apartment"})
+	// database.Where(HotelType{Name: "Tent"}).FirstOrCreate(&HotelType{Name: "Tent"})
+
+	// database.Where(RoomType{Name: "Standard"}).FirstOrCreate(&RoomType{Name: "Standard"})
+	// database.Where(RoomType{Name: "Superior"}).FirstOrCreate(&RoomType{Name: "Superior"})
+	// database.Where(RoomType{Name: "Deluxe"}).FirstOrCreate(&RoomType{Name: "Deluxe"})
+	// database.Where(RoomType{Name: "Suite"}).FirstOrCreate(&RoomType{Name: "Suite"})
+	// database.Where(RoomType{Name: "Tent 2 Adults"}).FirstOrCreate(&RoomType{Name: "Tent 2 Adults"})
+	// database.Where(RoomType{Name: "Tent 4 Adults"}).FirstOrCreate(&RoomType{Name: "Tent 4 Adults"})
+
+	// gender := []Gender{{
+	// 	GenderType: "male",
+	// }, {
+	// 	GenderType: "female",
+	// }, {
+	// 	GenderType: "unknown",
+	// }}
+	// for _, gender := range gender {
+	// 	db.Create(&gender)
+	// }
+	// prefix := []Prefix{{
+	// 	PrefixType: "Mr.",
+	// }, {
+	// 	PrefixType: "Mrs.",
+	// }, {
+	// 	PrefixType: "Ms.",
+	// }, {
+	// 	PrefixType: "Miss.",
+	// }, {
+	// 	PrefixType: "unknown",
+	// }}
+	// for _, prefix := range prefix {
+	// 	db.Create(&prefix)
+	// }
+	// country := []Country{{
+	// 	CountryName: "Loei",
+	// }, {
+	// 	CountryName: "Nakonrachsima",
+	// }, {
+	// 	CountryName: "unknown",
+	// }}
+	// for _, country := range country {
+	// 	db.Create(&country)
+	// }
+
+	// //bookignFlight
+	// foodFlight := []FoodFlight{{
+	// 	Maincourse: "nothing",
+	// 	Price:      0,
+	// }, {
+	// 	Maincourse: "padthai",
+	// 	Price:      200,
+	// }, {
+	// 	Maincourse: "hoytod",
+	// 	Price:      300,
+	// }}
+	// for _, foodFlight := range foodFlight {
+	// 	db.Create(&foodFlight)
+	// }
+
+	// baggeg := []Baggeg{{
+	// 	Weight: "nothing",
+	// 	Price:  0,
+	// }, {
+	// 	Weight: "50kg",
+	// 	Price:  50,
+	// }, {
+	// 	Weight: "100kg",
+	// 	Price:  100,
+	// }}
+	// for _, baggeg := range baggeg {
+	// 	db.Create(&baggeg)
+	// }
+
+	// //payment
+	// statuse := []PaymentStatus{{
+	// 	Status: "NO",
+	// }, {
+	// 	Status: "YES",
+	// }, {
+	// 	Status: "FAIL",
+	// }}
+	// for _, statuse := range statuse {
+	// 	db.Create(&statuse)
+	// }
+
+	// TypeName := []PaymentType{{
+	// 	TypeName: "Mobile Banking",
+	// }, {
+	// 	TypeName: "Promptpay",
+	// }}
+	// for _, TypeName := range TypeName {
+	// 	db.Create(&TypeName)
+	// }
+	// //company
+	// company := []Company{{
+	// 	CompanyName: "Airthailand",
+	// }, {
+	// 	CompanyName: "NokAirConditioning",
+	// }, {
+	// 	CompanyName: "ElysianAirlines",
+	// }}
+	// for _, company := range company {
+	// 	db.Create(&company)
+	// }
+
+	// //to
+	// to := []To{{
+	// 	ToName: "สุวรรณภูมิ",
+	// }, {
+	// 	ToName: "ภูเก็ต",
+	// }, {
+	// 	ToName: "เชียงใหม่",
+	// }}
+	// for _, to := range to {
+	// 	db.Create(&to)
+	// }
+
+	// //from
+	// from := []From{{
+	// 	FromName: "กระบี่",
+	// }, {
+	// 	FromName: "กรุงเทพ",
+	// }, {
+	// 	FromName: "หาดใหญ่",
+	// }}
+	// for _, from := range from {
+	// 	db.Create(&from)
+	// }
+
+	// newBooking := &BookFlight{}
+	// db.Create(newBooking)
+
+	// newBookingHotel := &BookHotel{}
+	// db.Create(newBookingHotel)
+
+}
---
+++ backend/test/unit/member_test.go
@@ -0,0 +1,62 @@
+package unit
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+	"github.com/Autsada55/P5VegProject480/entity"
+)
+
+func TestMemberfail(t *testing.T) {
+	g := NewGomegaWithT(t)
+	t.Run(`ต้องมากกว่า 7 แต่น้อยกว่า 100 ตัว`, func(t *testing.T) {
+		member := entity.Member{
+			Firstname:      "John",
+			Lastname:       "John",
+			Email:          "john@example.com",
+			Password:       "46", //password wrong
+		}
+
+		ok, err := govalidator.ValidateStruct(member)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(ContainSubstring("Password ต้องมากกว่า 7 แต่น้อยกว่า 100 ตัว"))
+	})
+
+	t.Run(`Email is invalid`, func(t *testing.T) {
+		member := entity.Member{
+			Firstname: "John",
+			Lastname:  "John",
+			Email:     "john", //email wrong
+			Password:  "12345678",
+		}
+
+		ok, err := govalidator.ValidateStruct(member)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(ContainSubstring("Email is invalid"))
+	})
+
+}
+
+func TestAllMember(t *testing.T) {
+	g := NewGomegaWithT(t)
+	t.Run(`All correct`, func(t *testing.T) {
+		member := entity.Member{
+			Firstname:      "John",
+			Lastname:       "John",
+			Email:          "john@example.com",
+			Password:       "46213456",
+		}
+		ok, err := govalidator.ValidateStruct(member)
+
+		g.Expect(ok).To(BeTrue())
+		g.Expect(err).To(BeNil())
+	})
+
+}
