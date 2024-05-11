ba5ab87c71531d465784a15803bc85ec0b820a48
Autsada555 <autsadaheha@gmail.com>
2024-05-11 15:12:28 +0700

    Navbarfix

--- frontend/src/pages/navbar/navbar.tsx
+++ frontend/src/pages/navbar/navbar.tsx
@@ -1,5 +1,6 @@
 
 import iconprofile from '../../assets/iconprofile.png'
+import iconsearch from '../../assets/search.png'
 
 const Navbar = () => {
 
@@ -11,18 +12,21 @@ const Navbar = () => {
 
                     <nav className="flex space-x-12 ">
                         <div>
-                            <input
-                                type="text"
-                                placeholder="search..."
-                                className="px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-                            />
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="search..."
+                                    className="w-[900px] px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+                                />
+                                <img className="absolute left-[1070px] top-7 w-8 h-8 " src ={iconsearch} alt = "iconS"/>
+                            </div>
                         </div>
                         <a href="#" className="text-white text-xl font-normal font-['Inter'] leading-9">Home</a>
                         <a href="#" className="text-white text-xl font-normal font-['Inter'] leading-9">Menu</a>
                         <a href="#" className="text-white text-xl font-normal font-['Inter'] leading-9">About</a>
                         <a href="#" className="text-white text-xl font-normal font-['Inter'] leading-9">Contract</a>
                         <div>
-                            <img className="absolute right-0 top-3 w-12 h-12 mr-4 mt-2" src={iconprofile} alt="icon" />
+                            <img className="absolute right-0 top-3 w-12 h-12 mr-4 mt-2" src={iconprofile} alt="iconP" />
                         </div>
                     </nav>
 
