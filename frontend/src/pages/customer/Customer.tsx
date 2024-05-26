c3ea649044862c61b7bac4e7cae6df2938db3154
chadawan9913 <b6409913@g.sut.ac.th>
2024-05-26 23:26:28 +0700

    customer ui

--- frontend/src/pages/customer/Customer.tsx
+++ frontend/src/pages/customer/Customer.tsx
@@ -1,14 +1,18 @@
 import Navbar from "../navbar/navbar";
 // import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
+
 export function Customer() {
 
     return (
         <>
         <Navbar/>
-        <div className="left-[181px] top-[157px] absolute text-black text-2xl font-bold font-['Inter']">Overview</div>
-        <div className="left-[181px] top-[195px] absolute text-black text-4xl font-bold font-['Inter']">User Profile</div>
-        <div className="w-[448px] h-[448px] bg-slate-300 ml-36 mt-44 rounded-3xl"></div>
+        <div>
+            <div className="left-[181px] top-[157px] absolute text-black text-2xl font-bold font-['Inter']">Overview</div>
+            <div className="left-[181px] top-[195px] absolute text-black text-4xl font-bold font-['Inter']">User Profile</div>
+            <div className="w-[448px] h-[448px] bg-slate-300 ml-36 mt-44 rounded-3xl"></div>
+        </div>
         </>
     )
 } 
+
