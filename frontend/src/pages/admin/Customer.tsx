9edf6b1ce192b65d6c6a618c2bc278b9a8ba0e0d
chadawan9913 <b6409913@g.sut.ac.th>
2024-05-28 16:12:17 +0700

    update

--- frontend/src/pages/admin/Customer.tsx
+++ frontend/src/pages/admin/Customer.tsx
@@ -5,8 +5,11 @@ export function Customer() {
     return (
         <>
         <Navbar/>
-        <div className="left-[181px] top-[187px] absolute text-black text-2xl font-bold font-['Inter']">Overview</div>
-        <div className="left-[181px] top-[225px] absolute text-black text-4xl font-bold font-['Inter']">User Profile</div>
+        <div>
+            <div className="left-[181px] top-[157px] absolute text-black text-2xl font-bold font-['Inter']">Overview</div>
+            <div className="left-[181px] top-[195px] absolute text-black text-4xl font-bold font-['Inter']">User Profile</div>
+            <div className="w-[448px] h-[448px] bg-slate-300 ml-36 mt-44 rounded-3xl"></div>
+        </div>
         </>
     )
 }
